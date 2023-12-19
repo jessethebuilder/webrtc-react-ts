9f38abe4acbfdacc5b3bb2c4c0ff57406eac9758
Jesse Farmer <jessethebuilder@yahoo.com>
2023-12-18 17:51:24 -0800

    Local Video

--- src/Room.tsx
+++ src/Room.tsx
@@ -1,19 +1,82 @@
-import React from 'react';
-import './App.css';
-import User from './User';
+import React, { useState, useRef, useEffect } from "react";
+import adapter from "webrtc-adapter";
+import "./App.css";
+import User from "./User";
 
 interface RoomProps {
-  userCount: number
+  userCount: number,
 }
 
+// let <RTCPeerConnection | null>peerConnection = null;
+// const peerConnection = new RTCPeerConnection();
+
 function Room({ userCount }: RoomProps) {
+  const [callState, setCallState] = useState("waiting");
+  const startButton = useRef<HTMLButtonElement>(document.createElement('button'));
+  const endButton = useRef<HTMLButtonElement>(document.createElement('button'));
+  // const peerConnection = useRef() as React.MutableRefObject<RTCPeerConnection | null>;
+
+  async function createPeerConnection() {
+    const peerConnection = new RTCPeerConnection();
+    // peerConnection.current = new RTCPeerConnection();
+
+    peerConnection.onicecandidate = (event) => {
+      const candidateData = {};
+
+      if (event.candidate) {
+        candidateData.candidate = event.candidate.candidate;
+        candidateData.sdpMid = event.candidate.sdpMid;
+        candidateData.sdpMLineIndex = event.candidate.sdpMLineIndex;
+      }
+    };
+
+    // peerConnection.ontrack = () => {
+    //   remoteVideo.srcObject = event.streams[0];
+    // }
+
+    // localStream.getTracks().forEach(track => pc.addTrack(track, localStream));
+  }
+
+  async function makeCall() {
+    await createPeerConnection();
+  }
+
+  function endCall() {
+    peerConnection.current = null;
+  }
+
+  useEffect(() => {
+    switch (callState) {
+      case "start":
+        startButton.current.disabled = true;
+        endButton.current.disabled = false;
+        makeCall();
+        break;
+      case "end":
+        startButton.current.disabled = false;
+        endButton.current.disabled = true;
+        endCall();
+        break;
+      case "waiting":
+        startButton.current.disabled = false;
+        endButton.current.disabled = true;
+        break;
+    }
+  }, [callState]);
+
   return (
     <div className="room">
       <header className="header">
         This Room has {userCount} Users.
       </header>
+
+      <User callerType="me" callState={callState} />
+
+      <button ref={startButton} onClick={(event) => setCallState("start")}>Start</button>
+      <button ref={endButton} onClick={(event) => setCallState("end")}>End</button>
     </div>
   );
 }
+
 
 export default Room;
--- src/User.tsx
+++ src/User.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import adapter from "webrtc-adapter";
 
 interface UserProps {
+  readonly callerType: string;
+  callState: string;
 }
 
-function User({ }: UserProps) {
+function User({ callerType, callState }: UserProps) {
+  const video = useRef<HTMLVideoElement>(document.createElement("video"));
+  const localStream = useRef() as React.MutableRefObject<MediaStream>;
+
+  async function start () {
+    localStream.current = await navigator.mediaDevices.getUserMedia({audio: false, video: true});
+    video.current.srcObject = localStream.current;
+  }
+
+  async function end() {
+    localStream.current.getTracks().forEach(track => track.stop());
+    video.current.srcObject = null;
+  }
+
+  useEffect(() => {
+    switch (callState) {
+      case "start":
+        if (callerType === "me") { start(); }
+        break;
+      case "end":
+      if (callerType === "me") { end(); }
+      break;
+    };
+  }, [callState]);
+
   return (
     <div className="user">
-
+      <video ref={video} playsInline={true} autoPlay={true}></video>
     </div>
   );
 }
